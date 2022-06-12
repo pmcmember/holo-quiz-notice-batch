17b1eabf2ab6e33e2f9ae27d0658158733937cc6
dev-fjk <fjk.develop@gmail.com>
2022-06-12 16:57:56 +0900

    Web hook URLにFIXMEコメント

--- src/slack_notify.py
+++ src/slack_notify.py
@@ -42,6 +42,7 @@ def slack_notify(total):
     # TODO MicroMicroCMSの管理用ページのURLが決まり次第URLを修正する
     _micro_cms_url = 'dummy'
 
+    # FIXME webhookのURLはネットで公開すると変更されてしまうので 環境変数などに入れるなどして対策する
     _slack_url = 'https://hooks.slack.com/services/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'
     slack_text = 'holo-quiz-notice-batch 処理終了\n' \
                  '\n' \
